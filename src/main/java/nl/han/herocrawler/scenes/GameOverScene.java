1c04b14fdf27c93f6271e1ed7123d65b9b67333c
sagj-vos <150244706+sagj-vos@users.noreply.github.com>
2024-03-21 10:11:23 +0100

    Added static scenes

---
+++ src/main/java/nl/han/herocrawler/scenes/GameOverScene.java
@@ -0,0 +1,15 @@
+package nl.han.herocrawler.scenes;
+
+import com.github.hanyaeger.api.scenes.StaticScene;
+
+public class GameOverScene extends StaticScene {
+    @Override
+    public void setupScene() {
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
+}
---
+++ src/main/java/nl/han/herocrawler/scenes/TitleScene.java
@@ -0,0 +1,15 @@
+package nl.han.herocrawler.scenes;
+
+import com.github.hanyaeger.api.scenes.StaticScene;
+
+public class TitleScene extends StaticScene {
+    @Override
+    public void setupScene() {
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
+}
