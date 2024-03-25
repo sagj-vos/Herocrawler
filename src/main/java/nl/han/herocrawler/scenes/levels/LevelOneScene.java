93cb0279e8201379d0a963e6ac0fb3ea97f7acaf
sagj-vos <150244706+sagj-vos@users.noreply.github.com>
2024-03-25 13:16:26 +0100

    added movable player

--- src/main/java/nl/han/herocrawler/scenes/levels/LevelOneScene.java
+++ src/main/java/nl/han/herocrawler/scenes/levels/LevelOneScene.java
@@ -1,7 +1,10 @@
 package nl.han.herocrawler.scenes.levels;
 
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
+import nl.han.herocrawler.entities.Player;
 import nl.han.herocrawler.scenes.tilemaps.TileMapLevelOne;
 
 public class LevelOneScene extends DynamicScene implements TileMapContainer {
@@ -12,7 +15,7 @@ public class LevelOneScene extends DynamicScene implements TileMapContainer {
 
     @Override
     public void setupEntities() {
-
+        addEntity(new Player("backgrounds/level-background.png", new Coordinate2D(400, 400), new Size(16, 16), 1, 1));
     }
 
     @Override
