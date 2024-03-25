8bf5ca048e9cf81a42250a2124a622d2f8ea669c
sagj-vos <150244706+sagj-vos@users.noreply.github.com>
2024-03-25 11:46:17 +0100

    added background resource

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module Herocrawler {
     requires hanyaeger;
 
     exports nl.han.herocrawler;
+    exports nl.han.herocrawler.entities.tiles;
+    exports nl.han.herocrawler.scenes.tilemaps;
 
+    opens backgrounds;
     opens tiles;
 }
