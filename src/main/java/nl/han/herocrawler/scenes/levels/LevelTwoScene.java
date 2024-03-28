70d962b1872ffa1bd71271e470083b59a3f56d34
jasper612 <j.kornelis1@student.han.nl>
2024-03-28 11:41:31 +0100

    added monsters to lvl 2

--- src/main/java/nl/han/herocrawler/scenes/levels/LevelTwoScene.java
+++ src/main/java/nl/han/herocrawler/scenes/levels/LevelTwoScene.java
@@ -5,7 +5,13 @@ import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
 import nl.han.herocrawler.Herocrawler;
 import nl.han.herocrawler.entities.Player;
+import nl.han.herocrawler.entities.monsters.Raas;
+import nl.han.herocrawler.entities.monsters.Skeleton;
+import nl.han.herocrawler.entities.monsters.Wizard;
+import nl.han.herocrawler.scenes.buttons.ExitButton;
 import nl.han.herocrawler.scenes.tilemaps.TileMapLevelTwo;
+import nl.han.herocrawler.scenes.ui.HpUi;
+import nl.han.herocrawler.scenes.ui.InventoryUi;
 
 public class LevelTwoScene extends DynamicScene implements TileMapContainer {
     private final Herocrawler herocrawler;
@@ -23,6 +29,20 @@ public class LevelTwoScene extends DynamicScene implements TileMapContainer {
     @Override
     public void setupEntities() {
         addEntity(this.player);
+        addEntity(new HpUi(new Coordinate2D(0, 0)));
+        addEntity(new InventoryUi(new Coordinate2D(0, (getHeight() / 12 * 10))));
+        addEntity(new ExitButton(new Coordinate2D(getWidth() / 5 * 4, getHeight() / 12), this.herocrawler));
+
+        addEntity(new Skeleton(new Coordinate2D(getWidth() / 16 * 3 , getHeight()/ 12 * 2)));
+        addEntity(new Skeleton(new Coordinate2D(getWidth() / 16 * 5 , getHeight()/ 12 * 9)));
+
+        addEntity(new Wizard(new Coordinate2D(getWidth() / 16 * 5 , getHeight()/ 12 * 2)));
+        addEntity(new Wizard(new Coordinate2D(getWidth() / 16 * 6 , getHeight()/ 12 * 7)));
+        addEntity(new Wizard(new Coordinate2D(getWidth() / 16 * 8 , getHeight()/ 12 * 2)));
+        addEntity(new Wizard(new Coordinate2D(getWidth() / 16 * 10 , getHeight()/ 12 * 3)));
+        addEntity(new Wizard(new Coordinate2D(getWidth() / 16 * 11 , getHeight()/ 12 * 8)));
+
+        addEntity(new Raas(herocrawler, new Coordinate2D(getWidth() / 16 * 13 , getHeight()/ 12 * 4)));
     }
 
     @Override
